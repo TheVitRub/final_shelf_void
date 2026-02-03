e0494da5bd812b69c25cd466213ccdd8e99eb849
ryabovva <reggg2004@mail.ru>
2026-02-03 11:56:55 +0700

    обычное изменение

--- learning/upload_dataset_to_network.py
+++ learning/upload_dataset_to_network.py
@@ -8,7 +8,7 @@ import shutil
 from pathlib import Path
 
 # Настройки путей
-LOCAL_DATASET_PATH = "learning/dataset/2026-02-02" # <--- МЕНЯТЬ ПАПКУ ТУТ
+LOCAL_DATASET_PATH = "learning/dataset/final" # <--- МЕНЯТЬ ПАПКУ ТУТ
 
 # Конфигурация сетевого диска
 NETWORK_BASE_PATH = Path(r"T:\Рябов\learning")
